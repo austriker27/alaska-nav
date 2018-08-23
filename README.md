722a37d1b6e2252c2053cb530f401d793cf3d448
David Lindahl <davidalindahl@gmail.com>
2018-08-23 08:31:10 -0700

    readme updates and cleaning up code, added scroll

--- README.md
+++ README.md
@@ -1,30 +1,22 @@
-## A Star Wars Character Search
-This is a very tiny web app that allows you to search for your favorite, or least favorite, star wars character. It uses [SWAPI](https://swapi.co/) as an API endpoint for your query. It will return data from SWAPI for the character search. Data includes the characters name, gender, hair color and eye color. A user can search for a character with any letters and then filter results by gender. With the current implementation the app only returns the first 10 results from SWAPI.
+<p align="center">
+  <img src="./images/alaska-airlines.svg"  height="80"/>
+</p>
+
+
+## Alaska Airlines - In Air App Redesign
+This is a redesign of a mock Alaska Airlines app for staff to use while on flights. I rebuilt it from scratch using Vanilla JavaScript, CSS and HTML. 
 
 ## Deployment
-This project uses continuous deployment via [Netlify](https://www.netlify.com/) to deploy from the `master` branch. Check it out [here](https://star-wars-search.netlify.com/).
+This project uses continuous deployment via [Netlify](https://www.netlify.com/) to deploy from the `master` branch. Check it out [here](https://alaskaair.netlify.com/).
 
 <a href="https://www.netlify.com">
-  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"  height="144"/>
+  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"  height="80"/>
 </a>
-
-## Tech/framework used
-- jQuery
-- Vanilla JavaScript
-- CSS (from scratch)
 
 <b>Built in</b>
 - [VS Code](https://code.visualstudio.com/)
+- Vanilla JS, CSS and HTML
 
-## API Endpoint
-This app is using the following API endpoint at SWAPI, where `${searchValue}` is the user's search query. You can go to the link in your browser to look at the details. 
-```
-https://swapi.co/api/people/?search=${searchValue}
-```
-
-## API Reference
-- SWAPI docs for people are [here](https://swapi.co/documentation#people).
-- SWAPI docs for searching are [here](https://swapi.co/documentation#search). 
 
 
 ## Tests
@@ -32,33 +24,22 @@ At this time there are no tests.
 
 ## How to use?
 
-![A screencaptured gif of how to use this website](https://cl.ly/461q3t3G3Z2z/Screen%20Recording%202018-02-21%20at%2001.06%20PM.gif)
+Visit [the website](https://alaskaair.netlify.com/) and browse around the website.
 
-It is pretty easy to get off the ground with this website. 
-1. Search for your favorite character
-2. Peruse the results
-3. Filter the results by gender if desired.
-4. Search again to your heart's content!
+## TODO: 
 
-## Future TODO
-- Build out the ability for more than 10 results to come back from SWAPI (Pagination on SWAPI's end)
-- Write tests!
-- Remove hair color or eye color if the API response for a character is 'N/A', 'none', etc
+- better icons in the bottom bar
+- animated greeting
+- make more dry, espcially in DOM manipulation (one example would be writing a function to toggle children in the various menus on click)
 
 ## Contribute
 Feel free to contribute! Just submit a PR and I'll take a look.
 
 ## Credits
-Thanks to Soylent for powering me through this project. 
+Thanks to [Steve Schoger](www.stevechoger.com) for the zondicons used in the bottom bar
 
 ## License
 MIT License is applicable here.
 
 MIT © [David A. Lindahl](www.davidalindahl.com)
 
-
-TODO: 
-
-- better icons in the bottom bar
-- animated greeting
-- make more dry, espcially in DOM manipulation (one example would be writing a function to toggle children in the various menus on click)
--- scripts/script.js
+++ scripts/script.js
@@ -16,9 +16,6 @@ let bottomChildren = allBottomBar.querySelectorAll(".bottom-nav > li");
 //-------------------------------------------------------------
 // hamburger variable
 //-------------------------------------------------------------
-// let hamburgerTarget = document.querySelector('.nav-toggle');
-// let overflowTarget = document.querySelector(".overflow-target");
-
 let hamburgerParent = document.querySelector('.overflow-target');
 let hamburgerChildren = document.querySelectorAll(".overflow-target > .nav-toggle");
 
@@ -59,7 +56,6 @@ let agCustomsTarget = document.querySelector("#agCustoms");
 let agCustomsParent = document.querySelector(".safety .parent-nav");
 let agCustomsChildren = document.querySelectorAll("#agCustoms .sub-item");
 
-
 //-------------------------------------------------------------
 // empty state when bottom bar clicked
 //-------------------------------------------------------------
@@ -94,7 +90,6 @@ overflowTarget.addEventListener('click', function () {
   })
 }, false);
 
-
 //-------------------------------------------------------------
 // event listeners for bottom bar - safety
 //-------------------------------------------------------------
@@ -110,7 +105,6 @@ safetyBottomBar.addEventListener('click', function () {
   safetyBottomBar.classList.toggle("active");
   safetyChildren.forEach(function (child) {
   child.classList.toggle("hide");
-  // child.classList.toggle("slide-up-fade-in");
 
   adminChildren.forEach(function (child) {
     child.classList.add("hide");
@@ -123,18 +117,6 @@ safetyBottomBar.addEventListener('click', function () {
   })
   }, false);
 });
-
-// let scheduleBottomBar = document.querySelector("#schedule-bottom-bar");
-// let scheduleParent = document.querySelector(".schedule");
-// let scheduleChildren = scheduleParent.querySelectorAll(".schedule > .parent-nav > .sub-item");
-
-// scheduleBottomBar.addEventListener('click', function () {
-//   scheduleBottomBar.classList.toggle("active");
-//   scheduleChildren.forEach(function (child) {
-//     child.classList.toggle("hide");
-    // child.classList.toggle("slide-up-fade-in");
-//   }, false);
-
 
 //-------------------------------------------------------------
 // event listener for bottom bar - admin menu
@@ -151,7 +133,6 @@ adminMenu.addEventListener('click', function () {
   adminMenu.classList.toggle("active");
   adminChildren.forEach(function (child) {
     child.classList.toggle("hide");
-    // child.classList.toggle("slide-up-fade-in");
   })
 
   safetyChildren.forEach(function (child) {
@@ -165,8 +146,6 @@ adminMenu.addEventListener('click', function () {
   })
 }, false);
 
-
-
 //-------------------------------------------------------------
 // event listeners for bottom bar - schedule
 //-------------------------------------------------------------
@@ -179,12 +158,10 @@ scheduleBottomBar.addEventListener('click', function () {
     child.classList.remove("opened");
   })
 
-
   scheduleBottomBar.classList.toggle("active");
 
   scheduleChildren.forEach(function (child) {
     child.classList.toggle("hide");
-    // child.classList.toggle("slide-up-fade-in");
   })
   safetyChildren.forEach(function (child) {
     child.classList.add("hide");
@@ -209,7 +186,6 @@ reportingTarget.addEventListener('click', function () {
   agCustomsChildren.forEach(function (child) {
     child.classList.add("hide");
   })
-  // reportingTarget.classList.toggle("menu-has-children")
 }, false);
 
 //-------------------------------------------------------------
@@ -223,76 +199,14 @@ agCustomsTarget.addEventListener('click', function () {
   reportingChildren.forEach(function (child) {
     child.classList.add("hide");
   })
-  // agCustomsTarget.classList.toggle("menu-has-children")
 }, false);
-
 
 //-------------------------------------------------------------
 // hamburger open on click
 //-------------------------------------------------------------
-
 overflowTarget.addEventListener('click', function () {
 
   hamburgerChildren.forEach(function (child) {
     child.classList.toggle("opened");
   })
 }, false);
-
-
-// hamburgerTarget.addEventListener('click', function (e) {
-//   this.classList.remove('opened');
-// });
-
-// overflowTarget.addEventListener('click', function () {
-
-//   hamburgerChildren.forEach(function (child) {
-//     child.classList.remove("opened");
-//   })
-// }, false);
-
-
-
-
-// let reportingTarget = document.querySelector("#reporting");
-// let reportingParent = document.querySelector(".sub-parent-nav");
-// let reportingChildren = reportingParent.querySelectorAll("#reporting > .sub-parent-nav > .sub-item");
-
-// scheduleBottomBar.addEventListener('click', function () {
-//   // remove active off all bottom nav bars
-//   bottomChildren.forEach(function (child) {
-//     child.classList.remove("active");
-//   })
-
-//   scheduleBottomBar.classList.toggle("active");
-
-//   scheduleChildren.forEach(function (child) {
-//     child.classList.toggle("hide");
-    // child.classList.toggle("slide-up-fade-in");
-//   })
-
-
-//-------------------------------------------------------------
-// listen for click on menu-has-children and toggle sub-item class
-//-------------------------------------------------------------
-
-// let 
-// let parentNav = document.querySelector(".sub-parent-nav");
-// let subItem = document.querySelectorAll(".sub-item");
-
-// parentNav.addEventListener('click', function () {
-//   console.log("parentNav clicked");
-// }, false);
-
-
-
-// let scheduleBottomBar = document.querySelector("#schedule-bottom-bar");
-// let scheduleParent = document.querySelector(".schedule");
-// let scheduleChildren = scheduleParent.querySelectorAll(".schedule > .parent-nav > .sub-item");
-
-// scheduleBottomBar.addEventListener('click', function () {
-//   scheduleBottomBar.classList.toggle("active");
-//   scheduleChildren.forEach(function (child) {
-//     child.classList.toggle("hide");
-    // child.classList.toggle("slide-up-fade-in");
-//   })
-// }, false);
